a84fd1e72a21ec0bd758678c47fe2ac767bff326
Abdulloh <sabirovaabdulloh2004@gmail.com>
2024-02-09 17:32:07 +0500

    Base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+  },
+  {
+    path: 'orders',
+    loadChildren: () => import('./orders/orders.module').then( m => m.OrdersPageModule)
+  },
 ];
 
 @NgModule({

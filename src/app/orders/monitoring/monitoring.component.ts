2f86f6e011d2dc63d0dcf4b5b3d11913ba365eee
Abdulloh PC <sabirovabdulloh2004@gmail.com>
2024-06-12 07:20:52 +0500

    v0.2.73

--- src/app/orders/monitoring/monitoring.component.ts
+++ src/app/orders/monitoring/monitoring.component.ts
@@ -6,9 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./monitoring.component.scss'],
 })
 export class MonitoringComponent  implements OnInit {
+  segmentValue:  string = 'sends'
 
   constructor() { }
 
   ngOnInit() {}
 
+  onPeriodSegmentChange() {
+    // this.loaderSrv.showLoader = true
+    // this.resultsSrv.getResults(this.segmentValue).subscribe((res: any) => {
+    //   this.results.products = res.products
+    //   this.results.products_total = res.products_total
+    //   this.results.docs = res.docs
+    //   this.results.docs_total = res.docs_total
+    //   this.loaderSrv.showLoader = false
+    // }, err => {
+    //   this.loaderSrv.showLoader = false
+    //   this.toats.presentToast('Не удалось загрузить данные', 'warning')
+    // })
+  }
 }
